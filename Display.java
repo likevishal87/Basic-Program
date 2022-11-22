565853514a66eb597f074ff74a555072ea302e35
Vicky Verma <likevishalverma@gmail.com>
2022-11-23 00:13:28 +0530

    Display Message

---
+++ Display.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.Assignment;
+
+//DisplayAMessage
+public class Display {
+
+	public static void main(String[] args) {
+		String message = ("Hello World");
+		System.out.println(message);
+	}
+
+}
