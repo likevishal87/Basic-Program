841ab935297657d47c764fd2f334d7f94561eb78
Vicky Verma <likevishalverma@gmail.com>
2022-11-23 00:20:28 +0530

    The Name Using CommandLine

---
+++ CommandLine.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.Assignment;
+
+import java.util.Scanner;
+
+//To get the name using the command line.
+
+public class CommandLine {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the String : ");
+		String a = scanner.nextLine();
+		System.out.println(a);
+
+	}
+
+}
